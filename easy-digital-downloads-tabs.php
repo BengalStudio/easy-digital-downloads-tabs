2c78b6e818245bce7021cc14c4a89645412b56b7
Mithun Biswas <bhoot.biswas@gmail.com>
2020-06-19 23:04:34 +0600

    Add initial files

---
+++ easy-digital-downloads-tabs.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Easy Digital Downloads Tabs
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     easy-digital-downloads-tabs
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Easy_Digital_Downloads_Tabs
+ */
+
+// Your code starts here.
